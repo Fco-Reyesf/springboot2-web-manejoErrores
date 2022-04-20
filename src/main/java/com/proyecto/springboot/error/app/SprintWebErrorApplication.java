e19fca513a11092efa9a2090efb3c1f3b673a1fa
Fco-Reyesf <freyes.info@gmail.com>
2022-04-19 22:58:20 -0400

    creacion proyecto errores

---
+++ src/main/java/com/proyecto/springboot/error/app/SprintWebErrorApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.springboot.error.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintWebErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintWebErrorApplication.class, args);
+	}
+
+}
